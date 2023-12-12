20236c28b79a37abb037b60ef2539f1b8dfc3cac
avnishyadav01 <106922392+avnishyadav01@users.noreply.github.com>
2023-12-12 22:39:45 +0530

    Method Hiding

---
+++ MethodHiding.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zero
+{
+    class random
+    {
+      public void ra()
+        {
+            Console.WriteLine("Base class");
+        }
+    }
+    class rupay :random
+    {
+        public new void ra() //Method hiding new keyword is optional program will also work without new 
+        {
+            
+            Console.WriteLine("child class");
+            base.ra(); // base.ra() calls the base class method
+        }
+        static void Main()
+        {
+            rupay no = new rupay();
+            no.ra();
+            Console.ReadLine();
+        }
+        
+    }
+}
