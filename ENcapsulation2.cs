7575c4a19d9237f9cb65e6a5bb027589c5f1e542
avnishyadav01 <106922392+avnishyadav01@users.noreply.github.com>
2023-12-12 22:40:20 +0530

    Encapsulation Part 2

---
+++ ENcapsulation2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dark
+{
+    internal class Red
+    {
+        int CustomerId;      // Encapsulation means that our field should always be private and there functions should bind
+                           // them .Binding the data members and methods means the encapsulation
+        string CustomerName;
+        public void set()   // Method for entering the data 
+            {
+            CustomerId=int.Parse(Console.ReadLine());
+            CustomerName=Console.ReadLine();
+            }
+        public void get()  // Method for showing the data 
+        {
+            Console.WriteLine(CustomerId); Console.WriteLine(CustomerName);
+        }
+        
+    }
+    class gh
+    {
+        static void Main() 
+        {
+            Red g = new Red();
+            g.set();
+            g.get();
+            Console.ReadLine();
+        }
+    }
+}
