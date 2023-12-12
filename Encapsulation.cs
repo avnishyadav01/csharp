5b7ffc4fea6566afb96eab94fe1facdf490905b7
avnishyadav01 <106922392+avnishyadav01@users.noreply.github.com>
2023-12-12 22:33:36 +0530

    Encapsulation

---
+++ Encapsulation.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Joker
+{
+    class Propertiesgetset
+    {
+        string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        
+        public string decx //get set properties
+            {get;set;
+              }
+    }
+    class Propertiesgetset1:Propertiesgetset
+    {
+        static void Main()
+        {
+            Propertiesgetset1 a = new Propertiesgetset1();
+            a.Name = "asa";
+            Console.WriteLine(a.Name);
+            Console.ReadLine();
+            a.decx = "asaaa";
+            Console.WriteLine(a.decx);
+            Console.ReadLine();
+        }
+        
+    }
+
+
+}
